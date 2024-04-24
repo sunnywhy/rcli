e68028ac6d797fcb3fc36611e867d4a3b614fec5
Wei Liu <wei@hireguide.com>
2024-04-24 14:44:02 -0400

    other: test the password strength

--- src/process/gen_pass.rs
+++ src/process/gen_pass.rs
@@ -1,4 +1,5 @@
 use rand::seq::SliceRandom;
+use zxcvbn::zxcvbn;
 
 const UPPER: &[u8] = b"ABCDEFGHJKLMNPQRSTUVWXYZ";
 // no I, O, avoid confusion
@@ -61,7 +62,12 @@ pub fn process_genpass(
 
     password.shuffle(&mut rng);
 
-    println!("{}", String::from_utf8(password)?);
+    let password = String::from_utf8(password)?;
+    println!("{}", password);
+
+    // output password strength in stderr
+    let estimate = zxcvbn(&password, &[])?;
+    eprintln!("Estimated strength: {}", estimate.score());
 
     Ok(())
 }
